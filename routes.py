8ca22c492ea19cec6dea3b35daa530ae719a37ff
sampsaoinonen <80003556+sampsaoinonen@users.noreply.github.com>
2022-02-06 23:29:22 +0200

    heroku mods in route.py

--- routes.py
+++ routes.py
@@ -47,33 +47,3 @@ def register():
         else:
             return render_template("error.html", error="Something went wrong. Registration was aborted!")
 
-@app.route("/new_ad", methods=["GET", "POST"]) 
-def new_ad():
-    if request.method == "GET":
-        return render_template("new_ad.html")
-    if request.method == "POST":
-        users.check_csrf(request.form["csrf_token"])
-        title = request.form["title"]
-        description = request.form["description"]
-        phone = request.form["phone"]
-        email = request.form["email"]
-        location = request.form["location"]
-        price = request.form["price"]        
-        if len(title) < 3 or len(password1) > 20:
-            flash("Title has to be between 5 and 20 characters!")
-            return redirect("/new_ad")
-        if len(description) < 3 or len(description) > 200:
-            flash("Username has to be between 3 and 200 characters!")
-            return redirect("/new_ad")
-        if len(phone) > 20:
-            flash("Phone number has to be under 20 characters!")
-            return redirect("/new_ad")
-        if len(email) > 50:
-            flash("Email address has to be under 20 characters!")
-            return redirect("/new_ad")
-        if len(location) > 50:
-            flash("Location has to be under 50 characters!")
-            return redirect("/new_ad")
-        if len(price) < 0 or len(price) > 9999999.99 :
-            flash("Price has to be between 0 and 9,999,999.99")
-            return redirect("/new_ad")
