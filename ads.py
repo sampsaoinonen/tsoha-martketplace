c36b977a2a67fd4b96a4aae2ab05fd4bcadfb664
sampsaoinonen <80003556+sampsaoinonen@users.noreply.github.com>
2022-02-07 04:25:57 +0200

    browse typo

--- ads.py
+++ ads.py
@@ -1,9 +1,9 @@
 from db import db
 
 def get_ads():
-    sql = '''SELECT A.id, A.title, A.description, A.phone, A.email, A.location, A.price, A.sent_at, A.user_id, C.cat_name FROM ads A, categories C
-    WHERE C.id=A.cat_id
-    GROUP BY A.id, C.cat_name
+    sql = '''SELECT A.id, A.title, A.description, A.phone, A.email, A.location, A.price, A.sent_at, U.username, C.cat_name FROM ads A, categories C, users U
+    WHERE C.id=A.cat_id AND U.id=A.user_id
+    GROUP BY A.id, C.cat_name, U.username
     ORDER BY A.sent_at'''
     result = db.session.execute(sql)
     return result.fetchall()
