96fb7642e04c3ff0df263ae30c77cd88a183e4c0
sampsaoinonen <sampsa.oinonen@gmail.com>
2025-03-01 14:45:25 +0200

    Fix circular import in db

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from os import getenv
+from db import db  
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+
+DATABASE_URL = getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 import routes
--- db.py
+++ db.py
@@ -1,10 +1,4 @@
-from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
-        "postgres://", "postgresql://", 1)
-        
-db = SQLAlchemy(app)
+db = SQLAlchemy()
