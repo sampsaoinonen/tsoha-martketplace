72e907d1f58c738b4d365dd9ae15d60209e38e99
sampsaoinonen <sampsa.oinonen@gmail.com>
2025-03-01 15:00:01 +0200

    Undo app.py and db.py changes

--- db.py
+++ db.py
@@ -1,4 +1,10 @@
+from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
+        "postgres://", "postgresql://", 1)
+        
+db = SQLAlchemy(app)
